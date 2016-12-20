e4c7393c93902d9cd700cd75c90c80862f46a984
jordan <j.schatzman@outlook.com>
2016-12-19 22:46:53 -0800

    adding new jinja files with link to home page

--- learning_journal_basic/views.py
+++ learning_journal_basic/views.py
@@ -1,13 +1,41 @@
 from pyramid.response import Response
 import io
 import os
-
-THIS_DIR = os.path.dirname(__file__)
+from pyramid.view import view_config
+# THIS_DIR = os.path.dirname(__file__)
+"""
 
 def home_page(request):
-    imported_text = open(os.path.join(THIS_DIR, 'sample.txt')).read()
-    file_date = io.open('data/sample.txt').read()
+    imported_text = io.open('/templates/index.html').read()
     return Response(imported_text)
+
+def new_entry(request):
+    imported_text = io.open('/templates/new_entry.html').read()
+    return Response(imported_text)
+
+def edit_entry(request):
+    imported_text = io.open('/templates/edit_entry.html').read()
+    return Response(imported_text)
+
+def journal_entry(request):
+    imported_text = io.open('/templates/journal_entry.html').read()
+    return Response(imported_text)
+
+"""
+
+"""
+@view_config(route_name='home', renderer='templates')
+def my_view(request):
+    #return {'project': 'learning_journal_basic'}
+    return 
+
+"""
+@view_config(route_name='home', renderer='/templates/index.jinja2')
+def home_page(request):
+    #return Response(imported_text)
+    return {'test': 'testvalue'}
+
+
 
 def includeme(config):
     config.add_view(home_page,
